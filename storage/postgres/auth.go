8d5594e4420d870c1fa9da01e472bed6617485b7
xamidullox <xamidulloxxamidullayev592>
2024-07-10 20:16:28 +0500

    auth

--- storage/postgres/auth.go
+++ storage/postgres/auth.go
@@ -3,9 +3,13 @@ package postgres
 import (
 	pb "auth_service/genproto/auth"
 	"auth_service/models"
+	"log"
+	"time"
 
 	"database/sql"
 	"errors"
+
+	"github.com/google/uuid"
 )
 
 type AuthRepo struct {
@@ -53,4 +57,43 @@ func (us *AuthRepo) Login(logreq *pb.UserLogin) (*models.User, error) {
 		return nil, errors.New("username or password incorrect")
 	}
 	return &user, nil
+}
+
+func (us *AuthRepo) UpdateProfile(user *pb.User) (*pb.Void, error) {
+	_, err := us.db.Exec("update users set full_name=$1, is_admin=$2, email=$3, password=$4, updated_at=$6 where id=$5", user.FullName, user.IsAdmin, user.Email, user.Password, user.Id, time.Now())
+	if err != nil {
+		return &pb.Void{}, err
+	}
+
+	return &pb.Void{}, nil
+
+}
+
+func (us *AuthRepo) DeleteProfile(rep *pb.Id) (*pb.Void, error){
+	_, err := uuid.Parse(rep.Id)
+	if err != nil {
+		log.Printf("Error parsing UUID: %v", err)
+		return &pb.Void{},err
+	}
+	_, err = us.db.Exec("update users set delete_at=$1 where id=$2", time.Now(), rep.Id)
+	if err != nil {
+		return &pb.Void{},err
+	}	
+	return &pb.Void{},nil
+}
+
+func (us *AuthRepo) ValidateUserId(rep *pb.Id) (*pb.Exists, error) {
+	query := `select 
+	            case 
+				    when id = $1 then true 
+				else 
+				    false 
+				end 
+			from 
+			    users 
+			where 
+			    id = $1 and deletad_at is null`
+	res := pb.Exists{}
+	err := us.db.QueryRow(query, rep.Id).Scan(&res.Exists)
+	return &res, err
 }
